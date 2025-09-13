f7565a114aeb6f16b86a3c0fff9f0d993f7e8127
Vedant Patil <vedantpatil@192.168.0.123>
2025-09-13 10:26:42 +0530

    added ollama to brewfile and git aliases to zsh

--- zsh/user/aliases.zsh
+++ zsh/user/aliases.zsh
@@ -1,8 +1,15 @@
 # Python Specific aliases
 alias python="python3.13"
 alias dac="deactivate"
-alias vac="python -m venv .venv && source .venv/bin/activate"
+alias vac="source .venv/bin/activate"
 #alias vac="source /Users/vedantpatil/Projects/.pm/shell-automations/scripts/activate_venv.sh"
+
+# Git aliases
+alias gpl="git pull origin"
+alias gph="git push origin"
+alias gc="git commit -am"
+alias ga="git add"
+alias gs="git status"
 
 # vim -> nvim
 alias vi="nvim"
